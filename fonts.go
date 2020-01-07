d32a85ebddfda099023ea4e82551b2afbb6e0739
marcetin <marcetin@gmail.com>
2020-01-07 13:29:40 +0100

    Initial

---
+++ fonts.go
@@ -0,0 +1,19 @@
+package main
+
+//func Register() {
+//	register(text.Font{}, bariolregular.TTF)
+//	register(text.Font{Style: text.Italic}, bariolregularitalic.TTF)
+//	register(text.Font{Weight: text.Bold}, bariolbold.TTF)
+//	register(text.Font{Style: text.Italic, Weight: text.Bold}, bariolbolditalic.TTF)
+//	register(text.Font{Weight: text.Medium}, bariollight.TTF)
+//	register(text.Font{Weight: text.Medium, Style: text.Italic}, bariollightitalic.TTF)
+//}
+//
+//func register(fnt text.Font, ttf []byte) {
+//	face, err := opentype.Parse(ttf)
+//	if err != nil {
+//		panic(fmt.Sprintf("failed to parse font: %v", err))
+//	}
+//	fnt.Typeface = "Go"
+//	font.Register(fnt, face)
+//}
